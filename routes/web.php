033acefd4287496baf4bad98dc9efc32b607d618
unknown <bayuxbca@gmail.com>
2023-01-20 09:52:34 +0700

    sample controller & routing

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('products', [ProductController::class, 'index'])->name('products.index');
+Route::get('products/create', [ProductController::class, 'create'])->name('products.create');
+Route::post('products', [ProductController::class, 'store'])->name('products.store');
+Route::get('products/{id}/edit', [ProductController::class, 'edit'])->name('products.edit');
+Route::get('products/{id}', [ProductController::class, 'update'])->name('products.update');
+Route::get('products/{id}', [ProductController::class, 'destroy'])->name('products.destroy');
