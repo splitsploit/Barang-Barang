2e15fb5a651af340a489841fc41b3378aeb489bb
unknown <bayuxbca@gmail.com>
2023-01-20 13:38:45 +0700

    defining relationship ( many to many )

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -13,4 +13,9 @@ class Product extends Model
     {
         return $this->belongsTo(Brand::class);
     }
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class);
+    }
 }
